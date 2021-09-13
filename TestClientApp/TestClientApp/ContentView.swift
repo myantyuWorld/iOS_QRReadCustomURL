6ed232948b6e2e5f245380070762f3481c167cb3
大馬裕一 <ohbay@daibayuuichinoMacBook-Air.local>
2021-09-13 14:43:43 +0900

    [ios] initial commit

---
+++ TestClientApp/TestClientApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestClientApp
+//
+//  Created by 大馬裕一 on 2021/09/13.
+//  Copyright © 2021 大馬裕一. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
